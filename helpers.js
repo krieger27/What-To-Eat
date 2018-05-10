314886c89e89d58b1a8754e020e573b3c4a117da
krieger27 <krieger@vols.utk.edu>
2018-05-10 11:41:50 -0500

    restaurant model

--- helpers.js
+++ helpers.js
@@ -1,18 +1,13 @@
 const fs = require('fs');
 
-// moment.js library for displaying dates
 exports.moment = require('moment');
 
-// Using Dump to debug 
 exports.dump = (obj) => JSON.stringify(obj, null, 2);
 
-// helper function to make static map
 exports.staticMap = ([lng, lat]) => `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=800x150&key=${process.env.MAP_KEY}&markers=${lat},${lng}&scale=2`;
 
-// inserting an SVG
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
-// Some details about the site
 exports.siteName = `What to Eat!`;
 
 exports.menu = [
