5b71376dfe7d92b7c03829aa023e93502d6b8caf
krieger27 <krieger@vols.utk.edu>
2018-05-14 23:40:53 -0500

    routes working for now but will need to update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const restaurantCtrl = require('../controllers/restaurantCtrl');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', restaurantCtrl.homePage);
+router.get('/add', restaurantController.addRestaurant);
+router.post('/add', catchErrors(storeController.createRestaurant));
 
 module.exports = router;
