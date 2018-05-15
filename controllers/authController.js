eb0ab4723cc9f979c4567d41c478c0e44851681a
krieger27 <krieger@vols.utk.edu>
2018-05-14 22:55:44 -0500

    user and auth controllers need to do view

---
+++ controllers/authController.js
@@ -0,0 +1,93 @@
+const passport = require('passport');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const promisify = require('es6-promisify');
+const mail = require('../handlers/mail');
+
+exports.login = passport.authenticate('local', {
+  failureRedirect: '/login',
+  failureFlash: 'Failed to Login!',
+  successRedirect: '/',
+  successFlash: 'You are logged in!'
+});
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'You are logged out!');
+  res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+  // checking to see if the user is authenticated
+  if (req.isAuthenticated()) {
+    next();
+    return;
+  }
+  req.flash('error', 'Sorry you must be logged in to do that!');
+  res.redirect('/login');
+};
+
+exports.forgot = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    req.flash('error', 'No account with that email exists.');
+    return res.redirect('/login');
+  }
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+  await user.save();
+  
+  const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+  await mail.send({
+    user,
+    filename: 'password-reset',
+    subject: 'Password Reset',
+    resetURL
+  });
+  req.flash('success', `You have been emailed a password reset link.`);
+  res.redirect('/login');
+};
+
+exports.reset = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  });
+  if (!user) {
+    req.flash('error', 'Password reset is invalid or has expired');
+    return res.redirect('/login');
+  }
+  // if user exists, go to rest password form
+  res.render('reset', { title: 'Reset your Password' });
+};
+
+exports.confirmedPasswords = (req, res, next) => {
+  if (req.body.password === req.body['password-confirm']) {
+    next(); 
+    return;
+  }
+  req.flash('error', 'Passwords do not match!');
+  res.redirect('back');
+};
+
+exports.update = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() }
+  });
+
+  if (!user) {
+    req.flash('error', 'Password reset is invalid');
+    return res.redirect('/login');
+  }
+
+  const setPassword = promisify(user.setPassword, user);
+  await setPassword(req.body.password);
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpires = undefined;
+  const updatedUser = await user.save();
+  await req.login(updatedUser);
+  req.flash('success', 'Your password has been reset!');
+  res.redirect('/');
+};
